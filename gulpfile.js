738bd1fea4d2b7a9cc61d7cffaf4a568a9a3cc09
Julian Hecker <julianheckerdev@gmail.com>
2019-02-21 18:45:35 -0500

    fixed gulp watch with documentation

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,69 @@ const browserSync = require('browser-sync').create();
 
 const paths = {
   html: {
-
-  },
-  sass: {
-    source: 'src/scss/*.+(scss|sass)',
-    destination: 'src/css/'
+    src: 'src/html/**/*.html',
+    dest: 'dist/'
   },
   css: {
-    source: 'src/css/*.css',
-    destination: 'dist/css/'
+    src: 'src/css/**/*.css',
+    dest: 'dist/css/'
   },
   js: {
-
-  }
+    src: 'src/js/**/*.js',
+    dest: 'dist/js'
+  },
+  pug: {
+    src: 'src/pug/**/*.pug',
+    dest: ''
+  },
+  scss: {
+    src: 'src/scss/**/*.+(scss|sass)',
+    dest: 'src/css/'
+  },
+  watch: 'dist/**/*.*'
 };
 
+function html() {
+  console.log("html");
+}
+
+function css() {
+  console.log('css');
+}
+
+function js() {
+  console.log('js');
+}
+
+function pug() {
+  console.log('pug');
+}
+
 function scss() {
-  return gulp.src(paths.sass.src)
+  return gulp.src(paths.scss.src)
     .pipe(gulpSass())
-    .pipe(gulp.dest(paths.sass.dest));
+    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+  console.log('before init');
+  browserSync.init({
+    server: {
+      baseDir: 'dist/'
+    }
+  });
+  console.log('after init');
+  gulp.watch(paths.html.src, html);
+  gulp.watch(paths.css.src, css);
+  gulp.watch(paths.js.src, js);
+  gulp.watch(paths.pug.src, pug);
+  gulp.watch(paths.scss.src, scss);
+  gulp.watch(paths.watch).on('change', browserSync.reload);
 }
 
 
-
-
 // exports.build = gulp.series();
-// exports.watch = gulp.series();
-exports.default = gulp.series(scss);
+exports.scss = gulp.series(scss);
+exports.watch = gulp.series(watch);
+exports.default = gulp.series(watch);
